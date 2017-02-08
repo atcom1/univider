b641aa4640447413c3cc5a0fdc577305acb8a6dc
taoyang <hackty@163.com>
2017-02-09 00:57:44 +0800

    update dependencies

--- setup.py
+++ setup.py
@@ -17,6 +17,14 @@ AUTHOR_EMAIL = "ty@puton.info"
 URL = "http://puton.info"
 VERSION = __import__(PACKAGE).__version__
 
+install_requires = [
+    'beautifulsoup4==4.5.3',
+    'elasticsearch==2.4.1',
+    'Flask>=0.10',
+    'redis-py-cluster==1.3.3',
+    'thrift==0.9.3',
+]
+
 setup(
     name=NAME,
     version=VERSION,
@@ -26,6 +34,7 @@ setup(
     author_email=AUTHOR_EMAIL,
     license="Apache License, Version 2.0",
     url=URL,
+    install_requires=install_requires,
     packages=find_packages(),
     include_package_data = True,
     entry_points={
---
+++ univider/indexer.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+from thrift.transport import TTransport
+from thrift.transport import TSocket
+from thrift.transport import THttpClient
+from thrift.protocol import TBinaryProtocol
+
+from univider.settings import hbase_host, hbase_port
+
+gen_py_path = os.path.dirname(__file__) + '/gen-py'
+sys.path.append(gen_py_path)
+from hbase import THBaseService
+from hbase.ttypes import *
+
+class Storager:
+
+    # create_namespace 'spider'
+    # create 'spider:cplatform', {NAME => 'w', VERSIONS => 1, TTL => 2592000, BLOCKCACHE => true}
+
+    host = hbase_host
+    port = hbase_port
+    framed = False
+
+    def save(self,key,url,title,content):
+
+        socket = TSocket.TSocket(self.host, self.port)
+        if self.framed:
+            transport = TTransport.TFramedTransport(socket)
+        else:
+            transport = TTransport.TBufferedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = THBaseService.Client(protocol)
+
+        transport.open()
+
+        table = "spider:cplatform"
+        if(url != None and url != ''  ):
+            put = TPut(row=key, columnValues=[TColumnValue(family="w",qualifier="u",value=url)])
+            client.put(table, put)
+        if(title != None and title != ''  ):
+            try:
+                put = TPut(row=key, columnValues=[TColumnValue(family="w",qualifier="t",value=title)])
+                client.put(table, put)
+            except UnicodeEncodeError,e:
+                put = TPut(row=key, columnValues=[TColumnValue(family="w",qualifier="t",value=title.encode('utf8'))])
+                client.put(table, put)
+
+        if(content != None and content != ''  ):
+            try:
+                put = TPut(row=key, columnValues=[TColumnValue(family="w",qualifier="c",value=content)])
+                client.put(table, put)
+            except UnicodeEncodeError,e:
+                put = TPut(row=key, columnValues=[TColumnValue(family="w",qualifier="c",value=content.encode('utf8'))])
+                client.put(table, put)
+
+        # print "Putting:", put
+
+        transport.close()
+
+    def read(self,key):
+
+        socket = TSocket.TSocket(self.host, self.port)
+        if self.framed:
+            transport = TTransport.TFramedTransport(socket)
+        else:
+            transport = TTransport.TBufferedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = THBaseService.Client(protocol)
+
+        transport.open()
+
+        table = "spider:cplatform"
+
+        get = TGet(row=key)
+        print "Getting:", get
+        result = client.get(table, get)
+
+        print "Result:", result
+
+        transport.close()
